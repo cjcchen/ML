1f68f81475a46cad4512f3d5290c7128d3def785
cjcchen <ickchenjunchao@gmail.com>
2018-05-14 00:07:16 +0800

    add file

---
+++ image_caption/predict.sh
@@ -0,0 +1,2 @@
+python predict.py --caption_path /home/tusimple/junechen/data/annotations/captions_train2014.json --image_path ~/junechen/data/train2014 --log_path ~/junechen/ml_data/cap6 --save_path ~/junechen//ml_data/model6/
+#python data.py /home/tusimple/junechen/data/annotations/captions_train2014.json ~/junechen/data/train2014
